0f44d8dac5659e89ffb5997ad97a0bd9b1ba7b1f
azizahma <azizah_ma@yahoo.co.uk>
2017-06-08 14:14:40 +0100

    Create var_arithmetic.py

---
+++ var_arithmetic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Variables and some arithmetic
+def variables_arithmetic(a, b):
+    """The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b."""
+    c=(a**2+b**2)
+    return c
+print(variables_arithmetic(991, 821))
