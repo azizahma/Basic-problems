af2709255dcab27660a0ba649bad62d21ea77e9a
azizahma <azizah_ma@yahoo.co.uk>
2017-06-08 14:16:03 +0100

    Create cond_loop.py

---
+++ cond_loop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env Python
+#Conditions and loops
+def cond_loop(a,b) :
+    """Returns the sum of all odd integers from a through b, inclusively. """
+    val = []
+    while a < b :
+        if a % 2 == 0 :
+            val.append(a+1)
+            a = a + 2
+        else :
+            a = a + 1
+    #print(val)
+    print(sum(val))
+
+cond_loop(4603, 8843) # works for any even odd pairs of numbers
