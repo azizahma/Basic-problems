e0d89af7f4936827bee8bcb76425bfee336fce2a
Azizah Asi <azizah_ma@yahoo.co.uk>
2017-12-21 18:05:27 +0000

    Add files via upload

---
+++ motif_DNA.py
@@ -0,0 +1,19 @@
+def motif_DNA(file):
+    with open(file) as f:
+        for idx,line in enumerate(iter(f)):
+            if idx+1 == 1:
+                s = line.strip() # removing white space at the same time
+            else:
+                t = line.strip() # removing white space at the same time
+        #print(s)
+        #print(t)
+        #print(len(t))
+
+        for i in range(0, len(s)+1):
+            j = i + len(t)
+            #print(s[i:j])
+            if s[i:j] == t:
+                #print(i+1)
+                print(i+1, end=' ')
+
+motif_DNA('file')
