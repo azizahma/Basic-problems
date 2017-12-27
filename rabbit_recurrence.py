1ceb86ab1c421c364520176813006614280197c3
Azizah Asi <azizah_ma@yahoo.co.uk>
2017-12-27 21:22:48 +0000

    Add files via upload

--- rabbit_recurrence.py
+++ rabbit_recurrence.py
@@ -1,9 +1,9 @@
-#Fibonacci - Recurrence relationship - but the problem given is multi-nacci sequence of order k
+#Fibonacci - Recurrence relationship - but the problem given is multi-nacci sequence of order k ## F(n) = F(n-1) + k* F(n-2_
 memo = {0:0, 1:1}
 def rabbit_fib(n,k):
-    '''The total number of rabbit pairs that will be present after nn months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of kk rabbit pairs '''
+    '''The total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs '''
     if not n in memo:
         memo[n] = rabbit_fib((n-1),k) + k*rabbit_fib((n-2), k)
     return memo[n]
 
-print(rabbit_fib(60,5))
+print(rabbit_fib(5,3))
