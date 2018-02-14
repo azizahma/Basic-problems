a69c655c722500d403a90af8684f5407ca46b377
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-02-14 01:19:16 +0000

    Add files via upload

--- gene_orders.py
+++ gene_orders.py
@@ -5,6 +5,7 @@
 import itertools
 from functools import reduce
 def gene_orders(n):
+    '''The total number of permutations of length nn, followed by a list of all such permutations (in any order).'''
     l = list(range(0+1,n+1))
     nper = reduce(lambda m,n:m*n, l)
     #The number of permutations of a list is the factorial of the length of the list, divided by the product of the factorials of the multiplicity of each element (since sets of repeated elements are permuted with no effect)
