9dff1e0caeac8e1cb4e59e3622a19c13352d6b28
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-02-18 20:35:38 +0000

    Add files via upload

---
+++ organizing_strings.py
@@ -0,0 +1,21 @@
+#Enumerating k-mers Lexicographically
+#Basically, I just use itertools permutations and product, then drop any duplicate elements
+import re
+import itertools
+def enumerate_k_mers(file):
+    with open(file) as f:
+        info = ' '.join(line.strip() for line in f)
+        alphabet = re.findall('[A-Z]',info)
+        alphabet = sorted(alphabet)
+        n = re.findall('\d*', info)
+        n = [ x for x in n if x is not '' ]
+        n = int(''.join(n))
+        r = [ x for x in itertools.permutations(alphabet,n) ]
+        r = [ ''.join(b) for b in r ]
+        rr = [ x for x in itertools.product(alphabet, repeat=n) ]
+        rr = [ ''.join(b) for b in rr ]
+        results = r + rr
+        results = sorted(list(set(results)))  # need to drop any duplicates as well!!
+        print('\n'.join(results))
+
+enumerate_k_mers('file')
