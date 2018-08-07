d764e589756a1c7749999130e4225b90c649f013
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-08-07 13:39:55 +0100

    Add files via upload

--- longest_common_substring.py
+++ longest_common_substring.py
@@ -1,22 +1,22 @@
 #Finding a Shared Motif - longest common sub-sequence
 
+import re
 def longest_common_sub(file):
     with open(file) as f:
-        d = f.readlines()[1::2]
-        d = [ x.replace('\n','') for x in d ]
-        shortest_len = min([ len(x) for x in d ])
-        shortest_seq = ''.join([ x for x in d if len(x) == shortest_len])
-        other_seq = [ x for x in d if x != shortest_seq ]
+        d = f.read().replace('\n','').split('>')
+        d = ''.join([ x for x in d if x is not '' ])
+        seq = re.findall('[C,G,A,T]*', d)
+        seq = [ x for x in seq if x is not '']
+        shortest_len = min([ len(x) for x in seq ])
+        shortest_seq = ''.join([ x for x in seq if len(x) == shortest_len])
+        other_seq = [ x for x in seq if x != shortest_seq ]
 
         # what is the longest sub sequence that is common in all other sequences
         for i in range(shortest_len,0,-1):
-            sub = shortest_seq[:i]
-            print(sub)
-            for x in other_seq:
-                print(i,x)
-                if x[:i] == sub:
-                    print(sub)
-                    break
+            for s in range(shortest_len-i+1):
+                sub = shortest_seq[s:s+i]
+                if all(sequence.find(sub) >= 0 for sequence in other_seq):
+                    return sub
 
-longest_common_sub('input')
+print(longest_common_sub('rosalind_lcsm.txt'))
 
