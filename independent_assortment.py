cb50e08580948bd9de9e78d6e5e50294476cd229
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-08-08 11:07:28 +0100

    Add files via upload

--- independent_assortment.py
+++ independent_assortment.py
@@ -3,7 +3,7 @@
 import math
 def xcy(x,y):
     f = math.factorial
-    return f(x) / ( f(y) * f(x-y) )
+    return f(x) / ( f(y) * f(x-y) ) #x choose y
 
 
 def mendel_2(file):
@@ -18,7 +18,7 @@ def mendel_2(file):
         P = 2 ** k # 2 offsprings from each organism in each generation
         probability = []
         for x in range(N,P+1): # x - for at least N organism of AaBb
-            prob = xcy(P,x) * (0.25**(x)) * ((1 -0.25)**(P-x))   # use combinatorial P C x * (0.25**2) * ((1 -0.25) ** 2) - Mendel 2nd law: prob of getting AaBb
+            prob = xcy(P,x) * (0.25**(x)) * ((1 -0.25)**(P-x))   # use combinatorial P C x * (0.25**x) * ((1 -0.25) ** (P-x) - Mendel 2nd law: prob of getting AaBb
                                                                 # from mating with AaBb (for all other genotypes) is always 0.25
             probability.append(prob)
         print(sum(probability))
