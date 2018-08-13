1b085c9bd834c261889c2c2be028debdc13d1221
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-08-13 19:58:42 +0100

    Add files via upload

--- rna_splice.py
+++ rna_splice.py
@@ -2,10 +2,17 @@
 
 import re
 def rna_splice(file):
+    """
+    Given: A DNA string s (of length at most 1 kbp) and a collection of substrings of s acting as introns. All strings are given in FASTA format.
+    Return: A protein string resulting from transcribing and translating the exons of s. (Note: Only one solution will exist for the dataset provided.) 
+    """
     with open(file) as f:
         d = f.read().strip().replace('\n','')
         d = re.findall('[A,C,G,T]*', d)
         d = [ x for x in d if x is not '' ]
-        print(d)
+        s = d[0]
+        introns = d[1:]
+        print(s)
+        print(introns)
 
 rna_splice('input')
