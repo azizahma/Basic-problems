26e63fe7072b076550c2bfe8289e7f54f592c8b8
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-08-16 11:23:14 +0100

    Add files via upload

--- longest_asc_desc_subsequence.py
+++ longest_asc_desc_subsequence.py
@@ -1,14 +1,19 @@
 # Longest Increasing Subsequence
 
-def longest_subsequence(file):
+with open('input') as f:
+    d = f.readlines()
+    d = [x.replace('\n', '') for x in d]
+    n = int(d[0])
+    p = d[1].split(' ')
+
+def longest_subsequence(n,p):
     """
-    Given: A positive integer n≤10000n≤10000 followed by a permutation ππ of length n.
+    Given: A positive integer n≤10000 followed by a permutation π of length n.
     Return: A longest increasing subsequence of ππ, followed by a longest decreasing subsequence of π.
     """
-    with open(file) as f:
-        d = f.readlines()
-        d = [ x.replace('\n','') for x in d ]
-        n = int(d[0])
-        p =
+    print(n)
+    print(p)
+    
 
-longest_subsequence('input')
+
+longest_subsequence(n,p)
