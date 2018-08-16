d5e0c3d9163814218dddb236d618b49f2752c8c8
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-08-16 20:43:23 +0100

    Add files via upload

--- shortest_superstring.py
+++ shortest_superstring.py
@@ -3,17 +3,31 @@
 import re
 import itertools
 def fragment_assembly(file):
+    """
+    Given: At most 50 DNA strings of approximately equal length, not exceeding 1 kbp, in FASTA format (which represent 
+    reads deriving from the same strand of a single linear chromosome).
+    The dataset is guaranteed to satisfy the following condition: there exists a unique way to reconstruct the entire 
+    chromosome from these reads by gluing together pairs of reads that overlap by more than half their length.
+    Return: A shortest superstring containing all the given strings (thus corresponding to a reconstructed chromosome). 
+    """
     with open(file) as f:
         d = f.read().strip().replace('\n','')
-        #dR = re.findall('Rosalind_[0-9]*', d)
-        d1 = re.findall('[A,C,G,T]*', d)
-        d1 = [ x for x in d1 if x is not '' ]
-        #dct = dict(list(zip(dR,d1)))
-        #print(list(itertools.permutations(dR,2)))
-        print(d1)
-        for x in d1:
-            for l in range(len(x),0,-1):
-                print(x[:l])
+        d = re.findall('[A,C,G,T]*', d)
+        d = [ x for x in d if x is not '' ]
+        min_l = int(len(d[0])/2)
+        print(d)
+
+        #results = []
+        for x in d:
+            for l in range(min_l, len(d[0])+1):
+                for i in range(len(x)):
+                    if len(x[i:i+l]) >= min_l:
+                        pattern = x[i:i+l]
+                        if any(sequence.find(pattern) >= 0 for sequence in [ y for y in d if y is not x ] ):
+                            print(pattern)
+                            print(x)
+                            #results.append(pattern)
+        #print(results)
 
 fragment_assembly('input')
 
