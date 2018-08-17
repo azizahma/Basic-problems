c5717e6325a75b816b03b8b9bdbd807581d9a8f9
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-08-17 12:52:09 +0100

    Add files via upload

--- shortest_superstring.py
+++ shortest_superstring.py
@@ -14,38 +14,19 @@ import itertools
 with open('input') as f:
     d = f.read().strip().replace('\n','')
     dk = re.findall('Rosalind_[0-9]*', d)
-    print(dk)
     dv = re.findall('[A,C,G,T]*', d)
     dv = [ x for x in dv if x is not '' ]
     dct = dict(list(zip(dk,dv)))
     length = int(sum([ len(x) for x in dv ])/len(dk))
-    print(length)
 
 def overlap(dct):
     pairs = list(itertools.permutations([k for k,v in dct.items()],2))
     for p in pairs:
-        three_prime = [ v for k,v in dct.items() if k == p[0] ]
-        five_prime = [ v for k,v in dct.items() if k == p[1] ]
-        
-
+        left = ''.join([ v for k,v in dct.items() if k == p[0] ])
+        right = ''.join([ v for k,v in dct.items() if k == p[1] ])
+        for i in range(length,0,-1):
+            l = left[i:]
+            r = right[:-i]
+            if l == r:
+                print(l,r)
 overlap(dct)
-
-
-
-
-
-
-        #results = []
-        # for x in d:
-        #     for l in range(min_l, len(d[0])+1):
-        #         for i in range(len(x)):
-        #             if len(x[i:i+l]) >= min_l:
-        #                 pattern = x[i:i+l]
-        #                 if any(sequence.find(pattern) >= 0 for sequence in [ y for y in d if y is not x ] ):
-        #                     print(pattern)
-        #                     print([ y for y in d if y is not x ])
-                            #results.append(pattern)
-        #print(results)
-
-
-
