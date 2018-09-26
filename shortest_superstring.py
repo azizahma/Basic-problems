47ac489f6610afe6299ac2d36e510fca00f8863e
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-09-26 14:01:07 +0100

    Add files via upload

--- shortest_superstring.py
+++ shortest_superstring.py
@@ -50,38 +50,41 @@ def get_all_o_lap(dct):
 import pandas as pd
 def prettify_o_lap(o_lap):
     df = pd.DataFrame(o_lap)
-    #df = df.fillna('-')
+    df = df.fillna('-')
     return df
     #print(df.fillna('-'))
 
 def find_first_read(o_lap): # via dict; based on 'it only has a good overlap (>5 is significant overlap) when positioned to the left of other reads'
-    '''to look for - 'which column (from prettify_o_lap) has no 'significant' overlap?'''
-    # df = prettify_o_lap(o_lap) # for visual purpose only
+    '''to look for - 'which row/column has no 'significant' overlap? (df changes row and column - 
+    via dictionary method'''
+
     results = []
     for k,v in o_lap.items():
         for i,j in v.items():
-            if j < 5:
-                results.append(k)
+            if j < 5: #
+                #results.append((k,i)) # checking purpose
+                results.append(i)
+
     results = dict( (x,results.count(x)) for x in set(results) )
     m = max([ v for k,v in results.items() ])
     first = ''.join([ k for k,v in results.items() if v == m ])
     return first
 
-def find_key_of_largest_value(dct):
-    #o_lap = get_all_o_lap(dct)
-    vals = []
-    for k,v in o_lap.items():
-        for i,j in v.items():
-            vals.append(j)
-    m = max(vals)
-    largest = [[ i for i,j in v.items() if j==m ] for k,v in o_lap.items() if [ i for i,j in v.items() if j==m ] !=[]]
-    return largest
+# def find_key_of_largest_value(dct):
+#     #o_lap = get_all_o_lap(dct)
+#     vals = []
+#     for k,v in o_lap.items():
+#         for i,j in v.items():
+#             vals.append(j)
+#     m = max(vals)
+#     largest = [[ i for i,j in v.items() if j==m ] for k,v in o_lap.items() if [ i for i,j in v.items() if j==m ] !=[]]
+#     return largest
 
-def find_order(first, o_lap):
-    '''Returns a list of read names in the order in which they represent the genomic sequence.'''
-    order = [first]
-    #name = order[-1]
-    if name == 
+# def find_order(first, o_lap):
+#     '''Returns a list of read names in the order in which they represent the genomic sequence.'''
+#     order = [first]
+#     #name = order[-1]
+#     if name ==
 
 
     # for k,v in o_lap.items():
@@ -95,12 +98,13 @@ def find_order(first, o_lap):
 
 o_lap = get_all_o_lap(dct)
 first = find_first_read(o_lap)
-largest = find_key_of_largest_value(dct)
-print(find_order(first, o_lap))
+#largest = find_key_of_largest_value(dct)
+#print(find_order(first, o_lap))
 
+print(dct)
 print(first)
 print(o_lap)
-print(largest)
+#print(largest)
 print(prettify_o_lap(o_lap))
 
 
