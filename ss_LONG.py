28aff6af77cec4dea8589ac4f68c33067c199334
Azizah Asi <azizah_ma@yahoo.co.uk>
2018-10-03 22:57:13 +0100

    Add files via upload

--- ss_LONG.py
+++ ss_LONG.py
@@ -15,9 +15,50 @@ def meanLength(file):
     length = sum([ len(v) for k,v in d.items()])/len(d)
     return length
 
+# d = read_data_fr_file('rosalind_long.txt')
+# #d = read_data_fr_file('input')
+# left = [v for k, v in d.items()][0]
+# right = [v for k, v in d.items()][1]
 
-#print(read_data_fr_file('rosalind_long.txt'))
-print(meanLength('rosalind_long.txt'))
+def getOlap(left, right):
+    results = []
+    for i in range(len(left)-1, 0, -1): # only need to iterate the len(left), as n the input data none of the reads are completely nested in another read
+        l = left[i:]
+        r = right[:-i]
+        if l == r:  # and len(l) >= 5:
+            results.append(len(l))
+        else:
+            results.append(0)
+    return max(results)
+
+def getAllOlap(d):
+    o_lap = {}
+    for lname in [k for k, v in d.items()]:
+        a = {lname: {}}
+        zz = {}
+        for rname in [k for k, v in d.items() if k is not lname]:
+            left = ''.join([v for k, v in d.items() if k == lname])
+            right = ''.join([v for k, v in d.items() if k == rname])
+            b = {rname: getOlap(left, right)}
+            zz.update(b)
+        a[lname] = zz
+        o_lap.update(a)
+    return o_lap
+
+def prettyPrint(dd):
+
+
+
+
+#print(meanLength('rosalind_long.txt'))
+#print(getOlap(left,right))
+o_lap = getAllOlap(read_data_fr_file('rosalind_long.txt'))
+#o_lap = getAllOlap(read_data_fr_file('input'))
+print(o_lap)
+for k,v in o_lap.items():
+    for i,j in v.items():
+        print(k, i)
+
 
 #print(read_data_fr_file('input'))
 #print(meanLength('input'))
